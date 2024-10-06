86f4ecb86cf9ec823988f46cf45a6b745fd0c7f3
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-06 13:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function fibonacci(num) {
-// your code here
-	if(num<=1) return num;
-	return fibonacci(num-2)+fibonacci(num-1);
+    if (num <= 1) return num;
+    return fibonacci(num - 2) + fibonacci(num - 1);
 }
-fibonacci(5);
+
+console.log(fibonacci(5)); // Output: 5
+
 
 module.exports = fibonacci;
