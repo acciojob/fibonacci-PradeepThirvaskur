9ef5f90a1f7025c770aad58fb0a8c25a90bdb7e1
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-07 13:03:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-function fibonacci(num) {
-    if (num === 0) return 0; // 0th Fibonacci number
-    if (num === 1) return 1; // 1st Fibonacci number
-    let a = 0, b = 1;
-    for (let i = 2; i <= num; i++) {
-        let next = a + b;
-        a = b;
-        b = next;
-    }
-    return a;
+ function fibo(num) {
+    if (num <= 1) return num;
+    return fibo(num - 1) + fibo(num - 2);
 }
 
 // Example usage:
-console.log(fibonacci(1)); // Output: 0
-console.log(fibonacci(5)); // Output: 3
+console.log(fibo(1)); // Output: 0
+console.log(fibo(5)); // Output: 3
 
 
 module.exports = fibonacci;
