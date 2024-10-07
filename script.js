7b30ea8c2203a4fb1dd89db88f29e489adaa6d17
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-07 12:58:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-ffunction fibonacci(num) {
+function fibonacci(num) {
     if (num === 0) return 0; // 0th Fibonacci number
     if (num === 1) return 1; // 1st Fibonacci number
     let a = 0, b = 1;
